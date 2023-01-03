14a392f13e80a4710348d965ea08ea4558d4fbb2
jaehyeon park <jhp1276@naver.com>
2023-01-04 03:36:04 +0900

    Controller 2023-01-04

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,10 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,18 +13,23 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
         @RequestMapping(value="/hello", method= RequestMethod.GET)
     public String getHello(){
+            LOGGER.info("getHello 메서드가 호출되었습니다.");
             return "Hello World";
         }
 
         @GetMapping(value = "/name")
         public String getName(){
+            LOGGER.info("getName 메서드가 호출되었습니다.");
             return "Flature";
         }
 
         @GetMapping(value="/variable1/{variable}")
-        public String getVariable1(@PathVariable String variable){
+        public String getVariable1(@PathVariable String variable) {
+            LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
             return variable;
         }
 
@@ -48,5 +57,13 @@ public class GetController {
         @GetMapping(value = "request3")
         public String getRequestParam3(MemberDto memberDto){
             return memberDto.toString();
+        }
+
+        @ApiOperation(value = "GET 메서드 예제", notes = "@RequestParam을 활용한 GET Method")
+    @GetMapping(value = "/request1/swagger")
+    public String getRequestParamSwagger(@ApiParam(value = "이름", required = true) @RequestParam String name,
+                                         @ApiParam(value = "이메일", required = true) @RequestParam String email,
+                                         @ApiParam(value = "회사", required = true) @RequestParam String organization){
+            return name + " " + email + " " + organization;
         }
 }
