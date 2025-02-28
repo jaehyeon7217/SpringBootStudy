dfa2755f57cf6d9851123b8b1b55cad491ea90e8
jaehyeon park <jhp1276@naver.com>
2025-02-28 15:41:50 +0900

    신규 프로젝트 추가(Spring-Hello-2)

---
+++ Inflearn/01. 스프링 입문/hello/src/main/java/hello_spring_2/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package hello_spring_2.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+         model.addAttribute("data", "hello!!"); // data는 thymeleaf에 data (templates - hello.html <- 리턴)
+         return "hello"; // <- templates 의 hello.html을 호출하는 거임
+        // 컨트롤러에서 리턴값으로 문자를 반환하면 viewResolver가 화면을 찾아서 처리한다.
+        // resources:templates/ ' + {ViewName} + '.html'을 찾음
+    }
+}
---
+++ Inflearn/01. 스프링 입문/hello/src/test/java/hello_spring_2/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package hello_spring_2.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
