2b60413575a8bb69aba4e1f93573cab650c08830
jaehyeon park <jhp1276@naver.com>
2025-03-27 05:31:58 +0900

    스프링 핵심원리 프로젝트 생성

---
+++ Inflearn/03. 스프링 핵심 원리/hello/core/src/main/java/hello/core/opder/OrderService.java
@@ -0,0 +1,5 @@
+package hello.core.opder;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPrice);
+}
---
+++ Inflearn/03. 스프링 핵심 원리/hello/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.opder.Order;
+import hello.core.opder.OrderService;
+import hello.core.opder.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L; // long 은 null 이 안됨
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10_000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
